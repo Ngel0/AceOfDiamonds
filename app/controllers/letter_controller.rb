b6470d284c5224fa16a74b87bea1c48b2735d011
Ngel0 <36593574+Ngel0@users.noreply.github.com>
2019-12-27 22:24:44 +0300

    moved neural.model to aws s3 to reduce slug size

--- app/controllers/letter_controller.rb
+++ app/controllers/letter_controller.rb
@@ -6,11 +6,13 @@ class LetterController < ApplicationController
   end
 
   def create
+    modelpath = 'lib/neural.model'
+    `py lib/download.py #{modelpath}` unless File.file? modelpath
     inpath = 'lib/text.txt'
     outpath = 'lib/textout.txt'
     session[:intext] = params[:intext]
     File.open(inpath, 'w'){ |file| file.write session[:intext] }
-    `py lib/script.py #{inpath} #{outpath}`
+    `py lib/script.py #{inpath} #{outpath} #{modelpath}`
     value = File.open(outpath, 'r'){ |file| file.read }
     session[:outtext] = value
     redirect_to '/letter/new'
